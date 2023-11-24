a5d7e44ce18d5da3f204cdc5a9ca4401d5d6b5b2
ccarl0 <carlo.cecco0@gmail.com>
2023-11-24 17:26:16 +0100

    Initializing Post page

---
+++ nasa_viewer/nasa_viewer/ViewModels/PostViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using nasa_viewer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasa_viewer.ViewModels
+{
+    internal partial class PostViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        List<NasaAPODRoot> nasaAPODs;
+    }
+}
